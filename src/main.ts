cde337168915fbcfce0fea85e4243e5544403783
Omarespo-dev <omaresposito21@gmail.com>
2025-06-17 17:02:44 +0200

    Milestone 3 added

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,69 @@ type Actress = Person & {
 
 
 
+// 📌 Milestone 3
+
+// GET /actresses/:id
+// http://localhost:3333/actresses${id}
+
+// La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+
+// Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta.
+
+// Crea una funzione getActress che, dato un id, effettua una chiamata a:
+async function getActress(id: number): Promise<Actress | null> {
+
+  try {
+
+    const response = await fetch(`http://localhost:3333/actresses/${id}`)
+    //gestione errore response
+    if (!response.ok) {
+      throw new Error("Errore HTTP" + response.status);
+    }
+
+    const obj: unknown = await response.json()
+
+    if(!isActress(obj)){
+      throw new Error("Formato dei dati non valito");
+    }
+
+    return obj
+  } catch (err) {
+    if(err instanceof Error){
+      console.error(`Errore del serve ${err.message}`)
+    }
+    
+    return null
+  }
+}
+
+//definisco type guard per obj
+function isActress(object: unknown): object is Actress {
+  if (
+    typeof object === 'object' &&
+    object !== null &&
+    'id' in object &&
+    typeof object.id === 'number' &&
+    'name' in object &&
+    typeof object.name === 'string' &&
+    'birth_year' in object &&
+    typeof object.birth_year === 'number' &&
+    'biography' in object &&
+    typeof object.biography === 'string' &&
+    'image' in object &&
+    typeof object.image === 'string' &&
+    (!('death_year' in object) || typeof object.death_year === 'number')
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+async function risolviPromise() {
+  const risultato = await getActress(1);
+  console.log(risultato);
+}
+
+risolviPromise()
+
