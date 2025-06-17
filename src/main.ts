13e4d665c3de33011bb70653cc452827b5ffa943
Omarespo-dev <omaresposito21@gmail.com>
2025-06-17 15:57:39 +0200

    Added Milestone 1

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// 📌 Milestone 1
+// Crea un type alias Person per rappresentare una persona generica.
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string
+}
+
